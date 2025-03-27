6a569e09723d3fac189cbc7076d2d8cd1849d27e
Kael LISSARAGUE <162634008+Aiyukoji@users.noreply.github.com>
2025-03-27 14:27:29 +0100

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -3,12 +3,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-private static final String URL = "jdbc:sqlite:BDD/IDLS_M.db"; // chemin relatif vers le fichier
+    private static final String URL = "jdbc:sqlite:BDD/IDLS_M.db"; // chemin relatif vers le fichier
 
     public static Connection getConnection() {
         try {
-            System.out.println("REUSSI");            
-            return DriverManager.getConnection(URL);
+            Connection conn = DriverManager.getConnection(URL);
+            System.out.println("Connexion réussie");
+            return conn;
         } catch (SQLException ex) {
             System.out.println("Erreur de connexion à la base de données : " + ex.getMessage());
             return null;
